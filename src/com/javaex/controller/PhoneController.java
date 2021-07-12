e023f431e254fe97805eccd305d6981e9cf29974
wonho <wonho951@gmail.com>
2021-07-12 10:50:08 +0900

    2021.07.12 수업내용(model2방식-phonebook2)

---
+++ src/com/javaex/controller/PhoneController.java
@@ -0,0 +1,26 @@
+package com.javaex.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/PhoneController")	//	<-- 요청을 할수 있는 주소가 있어야함.
+public class PhoneController extends HttpServlet {
+
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
