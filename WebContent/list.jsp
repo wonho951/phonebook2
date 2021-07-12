7c499500e88c8f978d82cf2e589cd56119c122b8
wonho <wonho951@gmail.com>
2021-07-12 11:19:20 +0900

    2021.07.12 수업내용(model2방식-phonebook2)

---
+++ WebContent/list.jsp
@@ -0,0 +1,33 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>전화번호 리스트</h1>
+	<p>입력한 정보 내역입니다.</p>
+
+	<table border = "1">
+		<tr>
+			<td>이름</td>
+			<td>이효리</td>
+		</tr>
+	
+		<tr>
+			<td>핸드폰</td>
+			<td>010-2222-2222</td>
+		</tr>
+	
+		<tr>
+			<td>회사</td>
+			<td>02-9999-9999</td>
+		</tr>
+	
+	</table>
+
+
+</body>
+</html>
--- src/com/javaex/controller/PhoneController.java
+++ src/com/javaex/controller/PhoneController.java
@@ -1,19 +1,33 @@
 package com.javaex.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/PhoneController")	//	<-- 요청을 할수 있는 주소가 있어야함.
+import com.javaex.dao.PhoneDao;
+import com.javaex.vo.PersonVo;
+
+@WebServlet("/pbc")	//	<-- 요청을 할수 있는 주소가 있어야함.
 public class PhoneController extends HttpServlet {
 
 	
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("컨트롤러");	//작업코드 만들기 전에 뭐라도 하나 찍어서 테스트
 		
+		//리스트 만들기
+		PhoneDao phoneDao = new PhoneDao();	//메모리에 Dao올린다.
+		List<PersonVo> personList = phoneDao.getPersonList();
+		
+		System.out.println(personList);
+		
+		
+		//html작업 --> jsp에게 시킨다.
 	}
 
 	
@@ -24,3 +38,6 @@ public class PhoneController extends HttpServlet {
 	}
 
 }
+
+
+//localhost:8088/phonebook2/pbc
