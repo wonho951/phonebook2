2dc4db84a5d92f09112863513c1ed3dd345db0aa
wonho <wonho951@gmail.com>
2021-07-13 15:08:37 +0900

    2021.07.13 phonebook2(model2 - static -> forword,redirect 빼기)

--- src/com/javaex/util/WebUtil.java
+++ src/com/javaex/util/WebUtil.java
@@ -21,6 +21,7 @@ public class WebUtil {
 	
 	public static void forword(HttpServletRequest request, HttpServletResponse response, String path) throws ServletException, IOException {
 		
+		//forword 자주 사용하니 static으로 올려두고 사용
 		RequestDispatcher rd = request.getRequestDispatcher(path);
 		rd.forward(request, response);
 		
@@ -29,6 +30,8 @@ public class WebUtil {
 	
 	//redirect
 	public static void redirect (HttpServletRequest request, HttpServletResponse response, String url) throws IOException{
+		
+		//redirect 자주 사용하니 static으로 올려두고 사용
 		response.sendRedirect(url);
 	}
 	
